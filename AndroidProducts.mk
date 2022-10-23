390727606000aebc602cc09d0a08e0149278af78
ArcG <115998816+Arctic-G@users.noreply.github.com>
2022-10-23 11:36:38 +0000

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/evolution_spes.mk
+    $(LOCAL_DIR)/arrow_spes.mk
 
 COMMON_LUNCH_CHOICES := \
-    evolution_spes-user \
-    evolution_spes-userdebug \
-    evolution_spes-eng
+    arrow_spes-user \
+    arrow_spes-userdebug \
+    arrow_spes-eng
